2756aedfa00fef87ece1cd78ec233ad42f42e771
zumrutuysal <49073360+zumrutuysal@users.noreply.github.com>
2021-02-13 19:53:12 +0300

    first commit

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>{ 
+                new Car {Id = 1, BrandId = 1, ColorId = 1, ModelYear = 2021, DailyPrice = 200000, Description = "Audi" },
+                new Car {Id = 2, BrandId = 2, ColorId = 2, ModelYear = 2020, DailyPrice = 90000, Description = "Tofaş" },
+                new Car {Id = 3, BrandId = 3, ColorId = 2, ModelYear = 1999, DailyPrice = 120000, Description = "Peugeot" },
+                new Car {Id = 4, BrandId = 4, ColorId = 1, ModelYear = 2010, DailyPrice = 140000, Description = "Mercedes" },
+                new Car {Id = 5, BrandId = 5, ColorId = 1, ModelYear = 2007, DailyPrice = 62000, Description = "Fiat" }
+            };
+        }
+
+        public void Add(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(z=>z.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _cars.Where(z => z.Id == id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car updateToCar = _cars.SingleOrDefault(z => z.Id == car.Id);
+            updateToCar.BrandId = car.BrandId;
+            updateToCar.ColorId = car.ColorId;
+            updateToCar.ModelYear = car.ModelYear;
+            updateToCar.DailyPrice = car.DailyPrice;
+            updateToCar.Description = car.Description;
+        }
+    }
+}
---
+++ ReCapUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ReCapUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var item in carManager.GetAll())
+            {
+                Console.WriteLine($"Id: {item.Id} - BrandId: {item.BrandId} - ColorId: {item.ColorId} - ModelYear: {item.ModelYear} - DailyPrice: {item.DailyPrice} - Description: {item.Description}");
+            }
+        }
+    }
+}
